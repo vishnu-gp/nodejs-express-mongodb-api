b3d313ac01752e37cae96b386b18942af11769f6
Vishnu Gp <vishnu.aassk@gmail.com>
2021-04-18 10:16:25 +0530

    Added server.js

---
+++ server.js
@@ -0,0 +1,14 @@
+require('dotenv').config()
+
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
+const db = mongoose.connection
+db.on('error', (error) => console.error(error))
+db.once('open', () => console.log('Connected to database'))
+
+app.use(express.json())
+
+app.listen(5000, () => console.log('Server started'))
